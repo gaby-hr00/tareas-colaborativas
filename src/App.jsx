6728a169840286e30f02be77bb23290124973d09
Gabriela <gaby-hr00@gmail.com>
2025-09-27 09:33:19 -0500

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Registro from "./components/registro";
 import Login from "./components/login";
-import Dashboard from "./components/list";
 import List from "./components/list";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -10,15 +9,7 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/registro" />} />
-        <Route path="/registro" element={<Registro />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/list" element={<List />} />
-      </Routes>
-    </Router>
-    <ToastContainer position="top-right" autoClose={3000} />
+    
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,34 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.jsx'
+import Login from './components/login.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
+import { Navigate } from 'react-router-dom'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/list"
+          element={
+            <PrivateRoute>
+              <App />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/login" />} />
+        {/* <Route path="/" element={<Navigate to="/registro" />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/list" element={<List />} /> */}
+      </Routes>
+      <ToastContainer position='top-right'  autoClose={3000} />
+    </AuthProvider>
+  </BrowserRouter>
+)
